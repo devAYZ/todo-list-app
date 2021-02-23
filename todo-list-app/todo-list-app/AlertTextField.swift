a3edc5dad17353d7067ba8834ad9310fdbd19151
devAYZ <ayokunle.fatokimi@decagon.dev>
2021-02-23 21:50:55 +0100

    reform add new list button's  alert message

--- todo-list-app/todo-list-app/AlertTextField.swift
+++ todo-list-app/todo-list-app/AlertTextField.swift
@@ -8,9 +8,9 @@
 import UIKit
 var todoAlert = UIAlertController()
  func addTaskButton() {
-     todoAlert = UIAlertController(title: "New Task", message: "Creaate new task", preferredStyle: .alert)
+     todoAlert = UIAlertController(title: "CREATE", message: "Type Your New Todo Item", preferredStyle: .alert)
     todoAlert.addTextField(configurationHandler: nil)
-    let alertAction = UIAlertAction(title: "Save task", style: .cancel, handler: { [] _ in
+    let alertAction = UIAlertAction(title: "Save Todo Item", style: .cancel, handler: { [] _ in
         guard let textfield = todoAlert.textFields?.first, let todoText = textfield.text, !todoText.isEmpty else { return }
     })
     todoAlert.addAction(alertAction)
