a06b89e92c28826ef4fddbbe2078444afe1fdcad
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-02-24 19:18:42 +0100

    changed title text font color

--- todo-list-app/todo-list-app/TodoListTableViewController.swift
+++ todo-list-app/todo-list-app/TodoListTableViewController.swift
@@ -17,6 +17,8 @@ class TodoListTableViewController: UITableViewController {
         super.viewDidLoad()
     
         navigationItem.title = "Todo-List"
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemOrange]
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemOrange]
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
         navigationItem.rightBarButtonItem?.tintColor = .systemOrange
